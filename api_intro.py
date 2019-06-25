b72a850d6b62a809c5ab0ef6e1c61f9261829554
Alex Ruef <ruef.alex@gmail.com>
2019-06-25 13:54:28 -0700

    renamed all variables from camel case to snake case

--- api_intro.py
+++ api_intro.py
@@ -30,23 +30,23 @@ def get_access_token(authUrl, id, secret):
 # Make get request for information about a person at osu
 # Read in ONID from user
 # requires access_token retrieved in get_access_token()
-def get_person(access_token, apiUrl):
-    responseData = []
-    while len(responseData) < 1:
+def get_person(access_token, api_url):
+    response_data = []
+    while len(response_data) < 1:
         onid = input('Enter Person\'s ONID: ')
 
         params = {'onid': onid}
         headers = {'Content-Type': 'application/json',
                    'Authorization': f'Bearer {access_token}'}
 
-        request = requests.get(apiUrl, params=params, headers=headers)
+        request = requests.get(api_url, params=params, headers=headers)
         response = request.json()
-        responseData = response['data']
-        if len(responseData) < 1:
+        response_data = response['data']
+        if len(response_data) < 1:
             print(f'No data found for \"{onid}\". '
                   f'Please try a different search query.')
 
-    return responseData
+    return response_data
 
 
 # Requests OSU directory information using api
@@ -55,8 +55,8 @@ def get_person(access_token, apiUrl):
 # returns data if the search query finds some.
 # If no data is found the user is asked again to enter a query
 def get_directory(access_token, apiUrl):
-    responseData = []
-    while len(responseData) < 1:
+    response_data = []
+    while len(response_data) < 1:
         query = input('Enter Directory Search Query: ')
 
         params = {'q': query}
@@ -65,25 +65,25 @@ def get_directory(access_token, apiUrl):
 
         request = requests.get(apiUrl, params=params, headers=headers)
         response = request.json()
-        responseData = response['data']
-        if len(responseData) < 1:
+        response_data = response['data']
+        if len(response_data) < 1:
             print(f'No data found for \"{query}\". '
                   f'Please try a different search query.')
     return response['data']
 
 
 if __name__ == '__main__':
-    configPath = sys.argv[1]
-    with open(configPath, 'r') as configFile:
+    config_path = sys.argv[1]
+    with open(config_path, 'r') as configFile:
         config = json.load(configFile)
-        personsUrl = config['api']['persons_url']
-        directoryUrl = config['api']['directory_url']
-        authUrl = config['oauth2']['auth_api_url']
-        clientId = config['oauth2']['client_id']
-        clientSecret = config['oauth2']['client_secret']
+        persons_url = config['api']['persons_url']
+        directory_url = config['api']['directory_url']
+        auth_url = config['oauth2']['auth_api_url']
+        client_id = config['oauth2']['client_id']
+        client_secret = config['oauth2']['client_secret']
 
-    access_token = get_access_token(authUrl, clientId, clientSecret)
-    directoryData = get_directory(access_token, directoryUrl)
+    access_token = get_access_token(auth_url, client_id, client_secret)
+    directory_data = get_directory(access_token, directory_url)
 
-    for directory in directoryData:
+    for directory in directory_data:
         print(directory['attributes']['firstName'])
