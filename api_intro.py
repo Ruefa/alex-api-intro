213ee89546dca07cb2dc0383411208279216786e
Alex Ruef <ruef.alex@gmail.com>
2019-06-25 09:38:31 -0700

    read input from console for users key and secret

--- api_intro.py
+++ api_intro.py
@@ -2,3 +2,7 @@
 # authorize and send requests using OSU's API
 personsUrl = "https://api.oregonstate.edu/v1/persons"
 authUrl = "https://api.oregonstate.edu/oauth2/token"
+
+def get_access_token():
+    key = raw_input("Enter Consumer Key: ")
+    secret = raw_input("Enter Consumer Secret: ")
