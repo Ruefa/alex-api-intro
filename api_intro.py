b4fcac92fa8c45149f13bdbab77c92f5e9b2a3e3
Alex Ruef <ruef.alex@gmail.com>
2019-06-25 09:38:51 -0700

    import requests library to send http requests

--- api_intro.py
+++ api_intro.py
@@ -1,5 +1,7 @@
 # api_intro.py
 # authorize and send requests using OSU's API
+import requests
+
 personsUrl = "https://api.oregonstate.edu/v1/persons"
 authUrl = "https://api.oregonstate.edu/oauth2/token"
 
