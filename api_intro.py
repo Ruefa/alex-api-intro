472a0e41ad2f29169b31c20a6342d9bc7ef4494c
Alex Ruef <ruef.alex@gmail.com>
2019-06-25 11:33:17 -0700

    changed double quotes to single quotes

--- api_intro.py
+++ api_intro.py
@@ -7,35 +7,36 @@ import requests
 # Request access_token from osu api
 # Read in consumer key and consumer secret from user
 def get_access_token():
-    key = input("Enter Consumer Key: ")
-    secret = input("Enter Consumer Secret: ")
+    key = input('Enter Consumer Key: ')
+    secret = input('Enter Consumer Secret: ')
 
-    data = {"client_id": key, "client_secret": secret,
-            "grant_type": "client_credentials"}
+    data = {'client_id': key, 'client_secret': secret,
+            'grant_type': 'client_credentials'}
     request = requests.post(authUrl, data=data)
     response = request.json()
 
-    return response["access_token"]
+    return response['access_token']
 
 
 # Make get request for information about a person at osu
 # Read in ONID from user
 # requires access_token retrieved in get_access_token()
 def get_person(access_token):
-    onid = input("Enter Person's ONID: ")
+    onid = input('Enter Person\'s ONID: ')
 
-    params = {"onid": onid}
-    headers = {"Content-Type": "application/json",
-               "Authorization": f"Bearer {access_token}"}
+    params = {'onid': onid}
+    headers = {'Content-Type': 'application/json',
+               'Authorization': f'Bearer {access_token}'}
 
     request = requests.get(personsUrl, params=params, headers=headers)
     response = request.json()
-    return response["data"]
+    return response['data']
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     access_token = get_access_token()
     personData = get_person(access_token)
 
     for person in personData:
-        print("Person's Name: " + person["attributes"]["firstName"])
+        print('Person\'s Name: ' + person['attributes']['firstName'])
