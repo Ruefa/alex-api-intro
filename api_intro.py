c750997090c0b94d74cd1594b8c8ca82cb4e5b94
Alex Ruef <ruef.alex@gmail.com>
2019-06-25 09:46:14 -0700

    created and send request for oauth accesss

--- api_intro.py
+++ api_intro.py
@@ -8,3 +8,9 @@ authUrl = "https://api.oregonstate.edu/oauth2/token"
 def get_access_token():
     key = input("Enter Consumer Key: ")
     secret = input("Enter Consumer Secret: ")
+
+    data = {"client_id": key, "client_secret": secret, "grant_type": "client_credentials"}
+    request = requests.post(authUrl, data=data)
+    response = request.json()
+
+    return response["access_token"]
